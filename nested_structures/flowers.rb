73fd925599a0a34bef54aebbb99914e1a36500c0
KathleenScriver <klescriver@gmail.com>
2019-11-20 15:34:52 -0700

    Adds flowers file.

---
+++ nested_structures/flowers.rb
@@ -0,0 +1,467 @@
+garden = [
+    {
+      :commonName => "african lily",
+      :genus => "agapanthus",
+      :family => "amaryllidaceae",
+      :plantType => "bulbs",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/08/Agapanthus_Postbloom-1024x812.jpg"
+    },{
+      :commonName => "bellflower",
+      :genus => "campanula",
+      :family => "campanulaceae",
+      :plantType => "annuals",
+      :heightCm => 7,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "gratitude",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/05/Pyrenean_Bellflower_Campanula_speciosa_14222640484-518x500.jpg"
+    },{
+      :commonName => "cornflower",
+      :genus => "centaurea",
+      :family => "asteraceae",
+      :plantType => "annuals",
+      :heightCm => 30,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "anticipation",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/08/Centaurea_cyanus_flower_korenbloem_1-700x500.jpg"
+    },{
+      :commonName => "tickseed",
+      :genus => "coreopsis",
+      :family => "asteraceae",
+      :plantType => "annuals",
+      :heightCm => 15,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "cheerfulness",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/08/MaxPixel.freegreatpicture.com-Summer-Yellow-Was-Yellow-Plant-Flowers-Coreopsis-2669068-700x500.jpg"
+    },{
+      :commonName => "montbretia",
+      :genus => "crocosmia",
+      :family => "iridaceae",
+      :plantType => "bulbs",
+      :heightCm => 60,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "confidence",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/08/Crocosmia_masoniorum_Montbretia_%E1%83%99%E1%83%A0%E1%83%9D%E1%83%99%E1%83%9D%E1%83%A1%E1%83%9B%E1%83%98%E1%83%90-700x500.jpg"
+    },{
+      :commonName => "carnation",
+      :genus => "dianthus",
+      :family => "caryophyllaceae",
+      :plantType => "annuals",
+      :heightCm => 10,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "admiration",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/01/17069136127_d1cb7bbccc_z.jpg"
+    },{
+      :commonName => "bleeding heart",
+      :genus => "dicentra",
+      :family => "papaveraceae",
+      :plantType => "perennials",
+      :heightCm => 90,
+      :sunNeeds => "partial sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/12/Dicentra_formosa_closeup3-700x500.jpg"
+    },{
+      :commonName => "lisianthus",
+      :genus => "eustoma",
+      :family => "gentianaceae",
+      :plantType => "annuals",
+      :heightCm => 15,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "medium",
+      :symobolizes => "charm",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/07/Lisianthus_aka_Eustoma_7214-700x500.jpg"
+    },{
+      :commonName => "lady's eardrop",
+      :genus => "fuchsia",
+      :family => "onagraceae",
+      :plantType => "shrubs",
+      :heightCm => 15,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "elegance",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/fuchsia-178711_1920-700x500.jpg"
+    },{
+      :commonName => "gardenia",
+      :genus => "gardenia",
+      :family => "rubiaceae",
+      :plantType => "shrubs",
+      :heightCm => 60,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "hard",
+      :symobolizes => "joy",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/white-gardenia-in-bloom-700x500.jpg"
+    },{
+      :commonName => "spider flower",
+      :genus => "grevillea",
+      :family => "proteaceae",
+      :plantType => "shrubs",
+      :heightCm => 50,
+      :sunNeeds => "full sun",
+      :waterNeeds => "low",
+      :maintenance => "low",
+      :symobolizes => "faith",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/01/Grevillea_plurijuga_Purple_Haze_1247-700x500.jpg"
+    },{
+      :commonName => "baby's breath",
+      :genus => "gypsophila",
+      :family => "caryophyllaceae",
+      :plantType => "annuals",
+      :heightCm => 15,
+      :sunNeeds => "full sun",
+      :waterNeeds => "low",
+      :maintenance => "low",
+      :symobolizes => "innocence",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/05/Gypsophila_cerastioides_1zz-700x500.jpg"
+    },{
+      :commonName => "amaryllis",
+      :genus => "hippeastrum",
+      :family => "amaryllidaceae",
+      :plantType => "bulbs",
+      :heightCm => 30,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "beauty",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/Amaryllis_hippeastrum_-_Candy_floss-700x500.jpg"
+    },{
+      :commonName => "hortensia",
+      :genus => "hydrangea",
+      :family => "hydrangeaceae",
+      :plantType => "climbers",
+      :heightCm => 60,
+      :sunNeeds => "partial sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/01/Hydrangea_macrophylla_002-700x500.jpg"
+    },{
+      :commonName => "jewelweed",
+      :genus => "impatiens",
+      :family => "balsaminaceae",
+      :plantType => "annuals",
+      :heightCm => 30,
+      :sunNeeds => "partial sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/12/Starr_070906-8887_Impatiens_hawkeri-700x500.jpg"
+    },{
+      :commonName => "morning glory",
+      :genus => "ipomoea",
+      :family => "convolvulaceae",
+      :plantType => "annuals",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "death",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/12/Ipomoea_Palmata_07318-1024x684.jpg"
+    },{
+      :commonName => "jasmine",
+      :genus => "jasminum",
+      :family => "oleaceae",
+      :plantType => "climbers",
+      :heightCm => 30,
+      :sunNeeds => "full sun",
+      :waterNeeds => "low",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/jasmine-flower-201730_1280-1024x682.jpg"
+    },{
+      :commonName => "edelweiss",
+      :genus => "leontopodium",
+      :family => "asteraceae",
+      :plantType => "perennials",
+      :heightCm => 30,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/05/Leontopodium_alpinum_RHu-1024x768.jpg"
+    },{
+      :commonName => "mallow",
+      :genus => "malva",
+      :family => "malvaceae",
+      :plantType => "annuals",
+      :heightCm => 150,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "health",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2019/03/Malva_sylvestris_flower_1-700x500.jpg"
+    },{
+      :commonName => "forget me not",
+      :genus => "myosotis",
+      :family => "boraginaceae",
+      :plantType => "perennials",
+      :heightCm => 25,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "hope",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/07/Myosotis_alpestris_6186528141-700x500.jpg"
+    },{
+      :commonName => "myrtle",
+      :genus => "myrtus",
+      :family => "myrtaceae",
+      :plantType => "shrubs",
+      :heightCm => 600,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "good fortune",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/05/Myrtus_communis_subsp._tarentina003-924x1024.jpg"
+    },{
+      :commonName => "catnip",
+      :genus => "nepeta",
+      :family => "lamiaceae",
+      :plantType => "perennials",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "low",
+      :maintenance => "low",
+      :symobolizes => "fertility",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/12/Nepeta_racemosa_Little_Titch_0zz.jpg"
+    },{
+      :commonName => "love-in-a-mist",
+      :genus => "nigella",
+      :family => "ranunculaceae",
+      :plantType => "annuals",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "harmony",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/07/nigella-damascena-365419_1920-1024x680.jpg"
+    },{
+      :commonName => "evening primrose",
+      :genus => "oenothera",
+      :family => "onagraceae",
+      :plantType => "annuals",
+      :heightCm => 15,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/12/Unidentified_Oenothera_flowers_buds-1024x875.jpg"
+    },{
+      :commonName => "balloon flower",
+      :genus => "platycodon",
+      :family => "campanulaceae",
+      :plantType => "perennials",
+      :heightCm => 60,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "friendship",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/11/Platycodon_grandiflorus-1-1024x768.jpg"
+    },{
+      :commonName => "primrose",
+      :genus => "primula",
+      :family => "primulaceae",
+      :plantType => "perennials",
+      :heightCm => 60,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "grace",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/01/20140313Primula_elatior3-1024x768.jpg"
+    },{
+      :commonName => "rose",
+      :genus => "rosa",
+      :family => "rosaceae",
+      :plantType => "climbers",
+      :heightCm => 60,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "medium",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/Small_Red_Rose-1024x768.jpg"
+    },{
+      :commonName => "scabious",
+      :genus => "scabiosa",
+      :family => "caprifoliaceae",
+      :plantType => "annuals",
+      :heightCm => 15,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/09/Scabiosa_columbaria_Butterfly_Blue_2-1024x820.jpg"
+    },{
+      :commonName => "lilac",
+      :genus => "syringa",
+      :family => "oleaceae",
+      :plantType => "shrubs",
+      :heightCm => 200,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "innocence",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/12/20170410Syringa_vulgaris4-700x500.jpg"
+    },{
+      :commonName => "tulip",
+      :genus => "tulipa",
+      :family => "liliaceae",
+      :plantType => "annuals",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "low",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/Yellow_tulips_at_Keukenhof-700x500.jpg"
+    },{
+      :commonName => "african daisy",
+      :genus => "ursinia",
+      :family => "asteraceae",
+      :plantType => "annuals",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "love",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/Ursinia_anthemoides_02-700x500.jpg"
+    },{
+      :commonName => "violet",
+      :genus => "viola",
+      :family => "violaceae",
+      :plantType => "annuals",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "faith",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/Viola_tricolor_flower-768x1024.jpg"
+    },{
+      :commonName => "wisteria",
+      :genus => "wisteria",
+      :family => "fabaceae",
+      :plantType => "climbers",
+      :heightCm => 180,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "hard",
+      :symobolizes => "immortality",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/11/Wisteria_-_Longwood_Gardens_-_DSC01003-700x500.jpg"
+    },{
+      :commonName => "immortelle",
+      :genus => "xeranthemum",
+      :family => "asteraceae",
+      :plantType => "annuals",
+      :heightCm => 30,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "cheerfulness",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/01/Xeranthemum_annuum-1024x768.jpg"
+    },{
+      :commonName => "daisy",
+      :genus => "bellis",
+      :family => "asteraceae",
+      :plantType => "perennials",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "innocence",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/01/Bellis_perennis_harilik_kirikakar_estonia-700x500.jpg"
+    },{
+      :commonName => "lily of the valley",
+      :genus => "convallaria",
+      :family => "asparagaceae",
+      :plantType => "bulbs",
+      :heightCm => 30,
+      :sunNeeds => "partial sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "happiness",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/10/Lily_of_the_valley_777-700x500.jpg"
+    },{
+      :commonName => "mountain avens",
+      :genus => "dryas",
+      :family => "rosaceae",
+      :plantType => "perennials",
+      :heightCm => 20,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "medium",
+      :symobolizes => "innocence",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/08/dryas-octopetala-117412_1920-1024x768.jpg"
+    },{
+      :commonName => "spurge",
+      :genus => "euphorbia",
+      :family => "euphorbiaceae",
+      :plantType => "annuals",
+      :heightCm => 30,
+      :sunNeeds => "full sun",
+      :waterNeeds => "low",
+      :maintenance => "low",
+      :symobolizes => "protection",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/01/Euphorbia_Milii_flowers-700x500.jpg"
+    },{
+      :commonName => "blue daisy",
+      :genus => "felicia",
+      :family => "asteraceae",
+      :plantType => "annuals",
+      :heightCm => 50,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "happiness",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2018/07/Feliciaamelloides-mult-1024x768.jpg"
+    },{
+      :commonName => "snowdrops",
+      :genus => "galanthus",
+      :family => "amaryllidaceae",
+      :plantType => "bulbs",
+      :heightCm => 25,
+      :sunNeeds => "full sun",
+      :waterNeeds => "average",
+      :maintenance => "low",
+      :symobolizes => "hope",
+      :imgUrl => "https://www.atozflowers.com/wp-content/uploads/2017/12/bloom-blossom-flora-46245-700x500.jpg"
+    }
+  ]
+  
+  # 1. Return an array of all of the common names sorted alphabetically
+  
+  
+  # 2. Return an array of all flowers that grow higher than 50 cm
+  
+  
+  # 3. Return the tallest flower
+  
+  
+  # 4. Return a hash where each key is the :family and each value is an array of the corresponding flowers
+  
+  
+  # 5. Puts the common names of all the flowers that are annuals and low :maintenance
+  
+  
+  # 6. Return an array of all of the :genus names without repeats
+  
+  
+  # 7. Return the count of all flowers that symbolize innocence
+  
+  
+  # 8. Return a hash where each key is the plant type and each value is an array of the corresponding flowers
+  
